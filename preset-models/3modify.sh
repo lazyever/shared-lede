ab65a3678b60a453c88a35f7f78cf25138a392ae
lazyever <106870322+lazyever@users.noreply.github.com>
2022-07-11 23:16:50 +0800

    Update 3modify.sh

--- preset-models/3modify.sh
+++ preset-models/3modify.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #修改登录IP
-sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.22.1/g' package/base-files/files/bin/config_generate
 #修改主机名
-sed -i 's/OpenWrt/Xiaomi-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/Rm-AC2100/g' package/base-files/files/bin/config_generate
 #修改型号显示
 sed -i 's/Xiaomi Redmi Router AC2100/Redmi AC2100/g' target/linux/ramips/dts/mt7621_xiaomi_redmi-router-ac2100.dts
 sed -i 's/Xiaomi Mi Router AC2100/Xiaomi AC2100/g' target/linux/ramips/dts/mt7621_xiaomi_mi-router-ac2100.dts
